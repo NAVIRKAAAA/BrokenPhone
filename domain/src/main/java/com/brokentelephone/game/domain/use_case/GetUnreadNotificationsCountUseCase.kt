f91f2b6d639e891d5a5b79d35151690ddf1b40e0
RuslanDev <eeeeboy227@gmail.com>
2026-05-03 12:31:17 +0300

    Fix: GetUnreadNotificationsCountUseCase

--- domain/src/main/java/com/brokentelephone/game/domain/use_case/GetUnreadNotificationsCountUseCase.kt
+++ domain/src/main/java/com/brokentelephone/game/domain/use_case/GetUnreadNotificationsCountUseCase.kt
@@ -1,11 +1,14 @@
 package com.brokentelephone.game.domain.use_case
 
 import com.brokentelephone.game.domain.repository.NotificationsRepository
+import com.brokentelephone.game.domain.user.AuthState
 import com.brokentelephone.game.domain.user.UserSession
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.distinctUntilChangedBy
+import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.flow.flatMapLatest
 
 class GetUnreadNotificationsCountUseCase(
@@ -14,7 +17,10 @@ class GetUnreadNotificationsCountUseCase(
 ) {
     @OptIn(ExperimentalCoroutinesApi::class)
     fun execute(): Flow<Int> {
-        return userSession.user.filterNotNull().distinctUntilChangedBy { it.readNotificationIds }
+        return userSession.user
+            .filterNotNull()
+            .filter { userSession.authState.firstOrNull() is AuthState.Auth }
+            .distinctUntilChangedBy { it.readNotificationIds }
             .flatMapLatest { user ->
                 repository.getUnreadNotificationsCount(user.id, user.readNotificationIds)
             }
